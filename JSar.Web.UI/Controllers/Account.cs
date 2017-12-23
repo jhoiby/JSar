1633e1f269efbcc3f78e20abd6ea9bca019844f1
James Hoiby <james@hoiby.com>
2017-12-23 11:35:51 -0700

    Rename MVC project.

---
+++ JSar.Web.UI/Controllers/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSar.Web.Mvc.Controllers
+{
+    public class Account
+    {
+    }
+}
