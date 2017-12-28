eb0456953c19470ba979e7520ab86b28ba8c041b
James Hoiby <james@hoiby.com>
2017-12-28 10:52:26 -0700

    Add basic Person and Company roots.

---
+++ JSar.Membership.Domain/Aggregates/Organization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSar.Membership.Domain.Aggregates
+{
+    public class Organization : IAggregateRoot
+    {
+        private string _name;
+
+        public Organization(string name)
+        {
+            if (name?.Length < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(name), "An organization requires a name.");
+            }
+
+            _name = name;
+        }
+
+        public string Name {  get { return _name;  } }
+    }
+}
---
+++ JSar.Membership.Domain/Aggregates/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSar.Membership.Domain.Aggregates
+{
+    public class Person : IAggregateRoot
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            if (FirstName?.Length < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(FirstName), "A person requires a first name.");
+            }
+
+            if (LastName?.Length < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(LastName), "A person requires a last name.");
+            }
+
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string FirstName { get { return _firstName; } }
+        public string LastName { get { return _lastName; } }
+    }
+}
