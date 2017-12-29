924a1a3d45f65ce2f255cf0951cf4a7bb6020ef1
James Hoiby <james@hoiby.com>
2017-12-29 15:05:13 -0700

    Add to CommonResult documentation.

--- JSar.Membership.Messages/Queries/GetUserByEmail.cs
+++ JSar.Membership.Messages/Queries/GetUserByEmail.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace JSar.Membership.Messages.Queries
-{
+{/// <summary>
+/// Gets an appliction user from the identity system.
+/// </summary>
+/// <return>AppUser</return>
     public class GetUserByEmail : Query<CommonResult>
     {
         public GetUserByEmail(string email, Guid messageId = default(Guid)) : base(messageId)
