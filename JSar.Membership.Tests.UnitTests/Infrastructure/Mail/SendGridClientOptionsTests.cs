1861df951c2e888f371d289d63255bb369fe2682
James Hoiby <james@hoiby.com>
2018-01-17 22:54:20 -0700

    Add SendGridClientOptions.

---
+++ JSar.Membership.Tests.UnitTests/Infrastructure/Mail/SendGridClientOptionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JSar.Web.UI.Infrastructure.Mail;
+using Xunit;
+
+namespace JSar.Membership.Tests.UnitTests.Infrastructure.Mail
+{
+    public class SendGridClientOptionsTests
+    {
+        // Arrange
+        private readonly string _apiKey = "849A2129-0D82-4E38-928C-7EF1627C5D85-849A2129-0D82-4E38-928C-7EF1627C5D85";
+
+        [Fact]
+        public void NewSendGridClientOptions_WithConstructorData_ReturnsCorrectProperties()
+        {
+            // Act
+            var options = new SendGridClientOptions(_apiKey);
+
+            // Assert
+            Assert.Same(_apiKey, options.ApiKey);
+        }
+
+        [Fact]
+        public void NewSendGridClientOptions_WithLateData_ReturnsCorrectProperties()
+        {
+            // Act
+            var options = new SendGridClientOptions();
+            options.ApiKey = _apiKey;
+
+            // Assert
+            Assert.Same(_apiKey, options.ApiKey);
+        }
+
+    }
+
+}
---
+++ JSar.Web.UI/Infrastructure/Mail/SendGridClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSar.Web.UI.Infrastructure.Mail
+{
+    public class SendGridClientOptions
+    {
+        public SendGridClientOptions(string apiKey = "")
+        {
+            ApiKey = apiKey;
+        }
+
+        public string ApiKey { get; set; }
+    }
+}
