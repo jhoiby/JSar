dcde473426ee3609b8b58c22aeb55e7a96aea59f
James Hoiby <james@hoiby.com>
2018-01-20 20:56:00 -0700

    Add minimum Client aggregate root.

---
+++ JSar.Membership.Tests.UnitTests/Domain/Aggregates/ClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JSar.Web.UI.Domain.Aggregates.Client;
+using Xunit;
+
+namespace JSar.Membership.Tests.UnitTests.Domain.Aggregates
+{
+    public class ClientTests
+    {
+        [Fact]
+        public void NewClient_CorrectConstructor_ReturnsCorrectProperties()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var companyId = Guid.NewGuid();
+
+            // Act
+            var client = new Client(companyId, id);
+
+            // Assert
+            Assert.Equal(id, client.Id);
+            Assert.Equal(companyId, client.CompanyId);
+        }
+    }
+}
---
+++ JSar.Web.UI/Domain/Aggregates/Client/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSar.Web.UI.Domain.Aggregates.Client
+{
+    public class Client : AggregateRoot
+    {
+        private readonly Guid _companyId;
+
+        public Client(Guid companyId, Guid id) : base(id)
+        {
+            _companyId = companyId;
+        }
+
+        public Guid CompanyId => _companyId;
+    }
+}
